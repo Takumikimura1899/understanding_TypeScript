14ea7d7baddbd75e8ae987005878814c23ea902f
Takumikimura141899 <kimura141899@gmail.com>
2021-11-04 14:35:21 +0900

    [add] unknown never

--- src/basicPractice/app.ts
+++ src/basicPractice/app.ts
@@ -1,35 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log('Result: ' + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult
-// combineValues = 3
-
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
-
-function sendRequest(data: string, cb: (response: any) => void) {
-  return cb({ data: 'Hi there!' });
-}
-
-sendRequest('send this!', (response) => {
-  console.log(response);
-  return true;
-});
+const result = generateError('エラーが発生しました', 500);
+console.log(result);
