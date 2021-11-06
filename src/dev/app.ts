258219ca8632f5e2ee329502e58e23f2b097216b
Takumikimura141899 <kimura141899@gmail.com>
2021-11-06 21:44:37 +0900

    [add] private

--- src/dev/app.ts
+++ src/dev/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,13 +9,28 @@ class Department {
   describe(this: Department) {
     console.log('Department:' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 
 accounting.describe();
+accounting.printEmployeeInformation();
+
+// 以下のコードのように外部からアクセスするとバリデーションなどの処理を無視してしまう恐れがあるので、
+// アクセス方法を内部のみに制御する。private デフォルトではpublic
+// accounting.employees[2] = 'Anna';
 
 const accountingCopy = { name: 'Dummy', describe: accounting.describe };
-accountingCopy.describe();
+// accountingCopy.describe();
