944425611821ce3725c1c57de3584006dba42604
Takumikimura141899 <kimura141899@gmail.com>
2021-11-16 07:11:05 +0900

    [add] imterface 関数型の定義

--- src/dev/app.ts
+++ src/dev/app.ts
@@ -1,3 +1,15 @@
+// type AddFn = (a:number,b:number) => number
+
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let addNum: AddFn;
+
+addNum = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 // インターフェースはオブジェクトがどのような形であるか定義するもの
 interface Named {
   readonly name: string;
