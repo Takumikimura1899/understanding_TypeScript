17ea19b72511c37e38015456e5aac1028dc837a8
Takumikimura141899 <kimura141899@gmail.com>
2021-11-17 07:51:29 +0900

    [add] optional

--- src/dev/app.ts
+++ src/dev/app.ts
@@ -12,7 +12,8 @@ addNum = (n1: number, n2: number) => {
 
 // インターフェースはオブジェクトがどのような形であるか定義するもの
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -20,20 +21,26 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 28;
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person();
 
 user1.greet('Hello,I am');
 console.log(user1);
