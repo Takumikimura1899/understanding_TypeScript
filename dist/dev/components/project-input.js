cff3178fb9982de4b00eb75e0d9913f94ff88aa7
Takumikimura141899 <kimura141899@gmail.com>
2021-11-27 23:40:20 +0900

    [add] asによる名前付け

--- dist/dev/components/project-input.js
+++ dist/dev/components/project-input.js
@@ -5,8 +5,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 import { Component } from './base-component.js';
-import { validate } from '../util/validation.js';
-import { autobind } from '../decorators/autobind.js';
+import * as Validation from '../util/validation.js';
+import { autobind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 export class ProjectInput extends Component {
     constructor() {
@@ -39,9 +39,9 @@ export class ProjectInput extends Component {
             min: 1,
             max: 1000,
         };
-        if (!validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(mandayValidatable)) {
+        if (!Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(mandayValidatable)) {
             alert('入力値が正しくありません。再度お試しください。');
             return;
         }
@@ -66,6 +66,6 @@ export class ProjectInput extends Component {
     }
 }
 __decorate([
-    autobind
+    Autobind
 ], ProjectInput.prototype, "submitHandler", null);
 //# sourceMappingURL=project-input.js.map
